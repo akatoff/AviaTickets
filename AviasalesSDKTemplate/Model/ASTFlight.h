04a08859d0dbb2b2838bb1a36889e8be03db1bd9
Ilya Amelchenkov <ilyaamelchenkov@MacBook-Pro-Ilya.local>
2016-04-15 12:04:23 +0300

    define types for relations

--- AviasalesSDKTemplate/Model/ASTFlight.h
+++ AviasalesSDKTemplate/Model/ASTFlight.h
@@ -1,5 +1,12 @@
 #import <Foundation/Foundation.h>
+#import "ASTAirline.h"
+#import "ASTAirport.h"
 
 
 @interface ASTFlight : NSObject <JRSDKFlight>
+
+@property (nonatomic, retain) ASTAirline *airline;
+@property (nonatomic, retain) ASTAirport *destinationAirport;
+@property (nonatomic, retain) ASTAirport *originAirport;
+
 @end
--- AviasalesSDKTemplate/Model/ASTFlightSegment.h
+++ AviasalesSDKTemplate/Model/ASTFlightSegment.h
@@ -1,5 +1,11 @@
 #import <Foundation/Foundation.h>
+#import "ASTAirline.h"
+#import "ASTFlight.h"
 
 
 @interface ASTFlightSegment : NSObject <JRSDKFlightSegment>
+
+@property (nonatomic, retain) ASTAirline *segmentAirline;
+@property (nonatomic, retain) NSSet <ASTFlight *> *unorderedFlights;
+
 @end
